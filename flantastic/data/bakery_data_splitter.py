a86cecc0b2bff55540f01d55b9c5266a670a5906
loic.martel@outlook.com <loic.martel@outlook.com>
2019-10-05 21:57:17 +0200

    bakery filter

--- flantastic/data/bakery_data_splitter.py
+++
@@ -1,5 +0,0 @@
-from fsplit.filesplit import FileSplit
-
-fs = FileSplit(file='flantastic/data/boulangeries.csv', splitsize=5000000, output_dir='flantastic/data/boulangeries_chunks') #5.9 Mio
-
-fs.split()
