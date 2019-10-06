f2cf266611d7a5c61f2d2ed37848b76d91e6d525
loic.martel@outlook.com <loic.martel@outlook.com>
2019-10-06 18:11:36 +0200

    tmp commit

--- flantastic/flantastic/data/bakeries_importer.py
+++ flantastic/flantastic/data/bakeries_importer.py
@@ -5,19 +5,19 @@ from flantastic.flantastic.models import Bakeries
 from .definitions import TMP_CSV, DELIMITER
 
 
-reader = csv.DictReader(open(TMP_CSV, 'rb'), delimiter=DELIMITER)
-for line in reader:
-    enseigne = str(line.pop("enseigne"))
-    codpos = str(line.pop("codpos"))
-    vmaj = str(line.pop("vmaj"))
-    vmaj1 = str(line.pop("vmaj1"))
-    vmaj2 = str(line.pop("vmaj2"))
-    vmaj3 = str(line.pop("vmaj3"))
-    datema = str(line.pop("datemaj"))
-    lat = float(line.pop("latitude"))
-    lng = float(line.pop("longitude"))
+def import_bakeries():
+    reader = csv.DictReader(open(TMP_CSV, 'rt'), delimiter=DELIMITER)
+    for line in reader:
+        enseigne = str(line.pop("enseigne"))
+        codpos = str(line.pop("codpos"))
+        vmaj = str(line.pop("vmaj"))
+        vmaj1 = str(line.pop("vmaj1"))
+        vmaj2 = str(line.pop("vmaj2"))
+        vmaj3 = str(line.pop("vmaj3"))
+        datemaj = str(line.pop("datemaj"))
+        lat = float(line.pop("latitude"))
+        lng = float(line.pop("longitude"))
 
-    Bakeries(enseigne=enseigne, geom=Point(lng, lat))
-    import pdb
-    pdb.set_trace()
-    # Bakeries.save()
+        res = Bakeries(enseigne=enseigne, geom=Point(lng, lat), datemaj=datemaj)
+        import pdb; pdb.set_trace()
+        Bakeries.save()
