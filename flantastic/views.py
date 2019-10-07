717013d81a295b312958e23f3a7c0106e635596e
loic.martel@outlook.com <loic.martel@outlook.com>
2019-10-07 18:41:39 +0200

    get closest bakeries using fake position. Comments added.

--- flantastic/views.py
+++ flantastic/views.py
@@ -3,7 +3,8 @@ from django.template import loader
 from django.shortcuts import render
 from djgeojson.serializers import Serializer as GeoJSONSerializer
 from django.core.serializers import serialize
-
+from django.contrib.gis.db.models.functions import Distance
+from django.contrib.gis.geos import Point
 from .models import Bakeries
 
 
@@ -12,9 +13,16 @@ def zoom_on_position(request):
     return render(request, 'maplayer.html', context)
 
 
-def bakeries_arround(request):#, longitude, latitude):
+def bakeries_arround(request):  # , longitude, latitude):
     """Get bakeries arround users and also ones filled
     """
-    q_set = Bakeries.objects.all()[0:100]
+    lattitude = 45.76894690751726
+    longitude = 4.83203358225112
+
+    user_pos = Point(longitude, lattitude, srid=4326)
+
+    q_set = Bakeries.objects.annotate(distance=Distance(
+        'geom', user_pos)).order_by('distance')[0:20]
+
     gjson = serialize('geojson', q_set, geometry_field="geom")
     return HttpResponse(gjson)
