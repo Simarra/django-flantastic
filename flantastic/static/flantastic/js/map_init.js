dd29274d3e2f272d9f0245f43f36f63908edd119
Simarra <loic.martel@outlook.Com>
2019-10-11 12:56:55 +0200

    django leaflet destroyed with success

--- flantastic/static/flantastic/js/map_init.js
+++ flantastic/static/flantastic/js/map_init.js
@@ -5,7 +5,7 @@ function onEachFeature(feature, layer) {
 }
 
 
-function insert_closest_bakeries_json(dataurl, map) {
+function set_closest_bakeries_json(dataurl, map) {
     // Download GeoJSON via Ajax
     fetch(dataurl).then(resp => {
         return resp.json();
@@ -19,21 +19,40 @@ function insert_closest_bakeries_json(dataurl, map) {
 
 
 
-function map_init(map, options) {
+function map_init() {
     //TODO: Need to change the init order:
     // 1. Init map font
     // 2. Get user position.
     // 3. THEN get closests bakeries
     // 4. Stylish the bakeries.
-    console.log(options);
-    var event = new CustomEvent('mapready');
+    let watercolor = L.tileLayer("https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.{ext}", { id: 'watercolor', attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors', 'subdomains': 'abcd', 'ext': 'jpg' })
+
+    let osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { id: 'osm', attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors' });
+
+    var map = L.map('mapid', {
+        zoom: 10,
+        layers: [watercolor]
+    });
+
+    var baseMaps = {
+        "watercolor": watercolor,
+        "osm": osm
+    };
+
+    var bakeries = L.layerGroup([]);
+    var overlayMaps = {
+        "Boulangeries": bakeries
+    };
+
+    L.control.layers(baseMaps, overlayMaps).addTo(map);
+
 
     //GEOLOC STUFF
     function onLocationFound(e) {
         var radius = e.accuracy / 2;
         console.log(e.latlng)
         L.marker(e.latlng).addTo(map)
-            // L.circle(e.latlng, radius).addTo(map);
+        L.circle(e.latlng, radius).addTo(map);
     }
 
 
@@ -44,6 +63,10 @@ function map_init(map, options) {
         maxZoom: 16
     });
 
-    map.addEventListener('mapready', insert_closest_bakeries_json(map, dataurl), false);
+    return map;
+
 
 }
+
+
+var map = map_init()
