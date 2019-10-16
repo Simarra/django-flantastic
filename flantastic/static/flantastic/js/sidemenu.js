8eb1cf915296b6959c5c8632a97c5af481fa946a
Simarra <loic.martel@outlook.Com>
2019-10-16 14:20:31 +0000

    first improvement of star system

--- flantastic/static/flantastic/js/sidemenu.js
+++ flantastic/static/flantastic/js/sidemenu.js
@@ -12,14 +12,31 @@ function closeNav() {
 function slot_markup_clicked(properties) {
     // 1. Show pannel
     openNav()
-        // 2. Update form
+        // 3. Update form
     document.getElementById("enseigne").value = properties.enseigne
     document.getElementById("commentaire").value = properties.commentaire
+
     document.getElementsByName("stars").value = 1
 }
 
 function slot_empty_map_clicked() {
     // 1. Delete forms
     // 2. Hide pannel
+
+}
+
+// TODO: Add an event listener on start system
+
+function star_rating() {
+
+}
+
+var taste_value = 0;
+
+var radios = document.getElementsByName("stars");
+
+for (let rad of radios) {
+    console.log("hi hi")
+    rad.addEventListener("click", function() { console.log(document.querySelector('input[name="stars"]:checked').value) })
 
 }
