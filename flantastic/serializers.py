33c84f8f8d2c585f8d6542e431f99f3d91e04d18
Simarra <loic.martel@outlook.Com>
2019-10-27 15:24:08 +0000

    dynamic taste updating

--- flantastic/serializers.py
+++ flantastic/serializers.py
@@ -5,20 +5,28 @@ from django.core.serializers import serialize
 
 def _update_dict_unsing_qset(dict_json: dict, q_set: QuerySet) -> dict:
     for feature in dict_json['features']:
-        for vote in q_set:
-            # Cache should be used
-            if int(feature["properties"]["pk"]) == int(vote.bakerie.id):
-                feature["properties"]["pate"] = vote.pate
-                feature["properties"]["texture"] = vote.texture
-                feature["properties"]["apparence"] = vote.apparence
-                feature["properties"]["commentaire"] = vote.commentaire
-                feature["properties"]["gout"] = vote.gout
-            else:
-                feature["properties"]["pate"] = None
-                feature["properties"]["texture"] = None
-                feature["properties"]["apparence"] = None
-                feature["properties"]["commentaire"] = None
-                feature["properties"]["gout"] = None
+        if len(q_set) > 0:
+            for vote in q_set:
+                # Cache should be used
+                if int(feature["properties"]["pk"]) == int(vote.bakerie.id):
+                    feature["properties"]["pate"] = vote.pate
+                    feature["properties"]["texture"] = vote.texture
+                    feature["properties"]["apparence"] = vote.apparence
+                    feature["properties"]["commentaire"] = vote.commentaire
+                    feature["properties"]["gout"] = vote.gout
+                else:
+                    feature["properties"]["pate"] = None
+                    feature["properties"]["texture"] = None
+                    feature["properties"]["apparence"] = None
+                    feature["properties"]["commentaire"] = None
+                    feature["properties"]["gout"] = None
+        else:
+            feature["properties"]["pate"] = None
+            feature["properties"]["texture"] = None
+            feature["properties"]["apparence"] = None
+            feature["properties"]["commentaire"] = None
+            feature["properties"]["gout"] = None
+            print(feature)
 
     return dict_json
 
