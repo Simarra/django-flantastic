85d45c551e3926a254b85dafcaa4b51e3d350f25
Simarra <loic.martel@outlook.Com>
2019-11-15 10:47:39 +0100

    Ft/packaging (#5)
    * pyproject v 0.1.0
    
    * remove docker from ci

--- mysite/settings.py
+++ mysite/settings.py
@@ -91,6 +91,12 @@ DATABASES = {
     }
 }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.contrib.gis.db.backends.spatialite',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
